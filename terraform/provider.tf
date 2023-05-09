955c23b140d95672e4ec00a3ffc1bdd7ce870a60
senges <charles.senges.io+github@protonmail.com>
2023-05-09 23:03:23 +0200

    hop(terraform): ajout du provider scaleway et import du projet pepouze.net

---
+++ terraform/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    scaleway = {
+      source = "scaleway/scaleway"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "scaleway" {
+  zone   = "fr-par-1"
+  region = "fr-par"
+}
+
+data scaleway_account_project "pepouze" {
+  name = "pepouze.net"
+}
